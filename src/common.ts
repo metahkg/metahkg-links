806e9e4710a3974f445863a84d5f13177e1e2342
wcyat <wcyat@wcyat.me>
2023-01-09 13:56:17 +0800

    use MONGO_URI to replace DB_URI

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
-export const mongouri = process.env.DB_URI || "mongodb://localhost";
+export const mongouri = process.env.MONGO_URI || "mongodb://localhost";
 export const client = new MongoClient(mongouri);
 export const metahkgDomain = process.env.metahkgDomain || "metahkg.org";
 export const db = client.db("metahkg");
