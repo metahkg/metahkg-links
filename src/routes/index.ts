bbac94686e29162b49c9033cfc5041a7bb06efd2
wcyat <wcyat@wcyat.me>
2023-01-27 02:12:30 +0800

    chore: code formatting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,11 @@ import { FastifyInstance, FastifyPluginOptions } from "fastify";
 
 export default function (
     fastify: FastifyInstance,
-    opts: FastifyPluginOptions,
+    _opts: FastifyPluginOptions,
     done: (e?: Error) => void
 ) {
     fastify.register(redirect);
-    fastify.get("/", async (req, res) => {
+    fastify.get("/", (_req, res) => {
         return res.code(301).redirect("https://gitlab.com/metahkg/metahkg-links");
     });
     done();
